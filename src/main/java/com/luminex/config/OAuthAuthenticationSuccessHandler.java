5b49796d4fd49fcbdab413fed15e4f04f08a90a2
Debasis Mishra <debasismishra000123@gmail.com>
2024-09-13 01:20:58 +0530

    github login added

--- src/main/java/com/luminex/config/OAuthAuthenticationSuccessHandler.java
+++ src/main/java/com/luminex/config/OAuthAuthenticationSuccessHandler.java
@@ -8,7 +8,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -35,7 +37,7 @@ public class OAuthAuthenticationSuccessHandler implements AuthenticationSuccessH
 
 	    logger.info("OAuthAuthenticationSuccessHandler triggered");
 
-	    DefaultOAuth2User oauthUser = (DefaultOAuth2User) authentication.getPrincipal();
+	    /*DefaultOAuth2User oauthUser = (DefaultOAuth2User) authentication.getPrincipal();
 
 	    String email = oauthUser.getAttribute("email").toString();
 	    String name = oauthUser.getAttribute("name").toString();
@@ -63,8 +65,72 @@ public class OAuthAuthenticationSuccessHandler implements AuthenticationSuccessH
 	    } else {
 	        logger.info("User already exists: " + email);
 	    }
+*/
+	    // identify the provider
+
+        var oauth2AuthenicationToken = (OAuth2AuthenticationToken) authentication;
+
+        String authorizedClientRegistrationId = oauth2AuthenicationToken.getAuthorizedClientRegistrationId();
+
+        logger.info(authorizedClientRegistrationId);
+
+        //get the details of login information
+        var oauthUser = (DefaultOAuth2User) authentication.getPrincipal();
+
+        oauthUser.getAttributes().forEach((key, value) -> {
+            logger.info(key + " : " + value);
+        });
+        User user = new User();
+        user.setUserId(UUID.randomUUID().toString());
+        user.setRoleList(List.of(AppConstants.ROLE_USER));
+        user.setEmailVerfied(true);
+        user.setEnebled(true);
+        user.setPassword("kunu");
+
+        if (authorizedClientRegistrationId.equalsIgnoreCase("google")) {
+
+            // google
+            // google attributes
+
+            user.setEmail(oauthUser.getAttribute("email").toString());
+            user.setProfilePic(oauthUser.getAttribute("picture").toString());
+            user.setName(oauthUser.getAttribute("name").toString());
+            user.setProviderUserId(oauthUser.getName());
+            user.setProvider(Providers.GOOGLE);
+        } else if (authorizedClientRegistrationId.equalsIgnoreCase("github")) {
+
+            // github
+            // github attributes
+            String email = oauthUser.getAttribute("email") != null ? oauthUser.getAttribute("email").toString()
+                    : oauthUser.getAttribute("login").toString() + "@gmail.com";
+            String picture = oauthUser.getAttribute("avatar_url").toString();
+            String name = oauthUser.getAttribute("login").toString();
+            String providerUserId = oauthUser.getName();
+
+            user.setEmail(email);
+            user.setProfilePic(picture);
+            user.setName(name);
+            user.setProviderUserId(providerUserId);
+            user.setProvider(Providers.GITHUB);
+        }
+
+        else if (authorizedClientRegistrationId.equalsIgnoreCase("linkedin")) {
+
+        }
+
+        else {
+            logger.info("OAuthAuthenicationSuccessHandler: Unknown provider");
+        }
+
+        User user2 = userRepo.findByEmail(user.getEmail()).orElse(null);
+        if (user2 == null) {
+            userRepo.save(user);
+            System.out.println("user saved:" + user.getEmail());
+        }
 
 
-        response.sendRedirect("/user/profile");
+
+        new DefaultRedirectStrategy().sendRedirect(request, response, "/user/profile");
+
     }
 }

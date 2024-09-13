fab83cdffcc8fab57d39d23e7f2820e92a76441f
Debasis Mishra <debasismishra000123@gmail.com>
2024-09-13 20:55:35 +0530

    updated

--- src/main/java/com/luminex/controller/UserController.java
+++ src/main/java/com/luminex/controller/UserController.java
@@ -1,21 +1,33 @@
 package com.luminex.controller;
 
+import java.security.Principal;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.luminex.helpers.Helper;
 
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 	
+	private Logger logger=LoggerFactory.getLogger(UserController.class);
+	
 	@RequestMapping(value = "/dashboard")
 	public String userDashboard() {
 		return "user/dashboard";
 	}
 	@RequestMapping(value = "/profile")
-	public String userProfile() {
+	public String userProfile(Authentication authentication) {
+		
+		String username=Helper.getEmailOfLoggedinUser(authentication);
+		logger.info("User logged in:{}",username);
 		return "user/profile";
 	}
 	

4f789cbbdd0a1bc56a47b7c2b8ce8896f29dfaf6
Debasis Mishra <debasismishra000123@gmail.com>
2024-10-05 20:09:57 +0530

    email Send Complete

--- src/main/java/com/luminex/repositories/UserRepo.java
+++ src/main/java/com/luminex/repositories/UserRepo.java
@@ -11,4 +11,5 @@ import com.luminex.entities.User;
 public interface UserRepo extends JpaRepository<User, String>{
 	Optional<User> findByEmail(String email);
 	Optional<User> findByEmailAndPassword(String email,String password);
+	Optional<User> findByEmailToken(String emailToken);
 }
